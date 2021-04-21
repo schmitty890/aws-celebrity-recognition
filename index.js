a63d97407f6fd4a2ec12cfe8971b5691e3f95104
Jason Schmitt <schmitty890@gmail.com>
2021-04-21 14:06:25 -0400

    checking for file type and size

--- index.js
+++ index.js
@@ -61,51 +61,59 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.post("/images", upload.single("image"), async (req, res) => {
   try {
     const file = req.file;
-    const result = await uploadFile(file);
-    await unlinkFile(file.path);
-    try {
-      AWS.config.setPromisesDependency();
-      //AWS access details
-      AWS.config.update({
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
-        region: process.env.AWS_BUCKET_REGION,
-      });
+    if (req.file.mimetype == "image/jpeg" && req.file.size >= 400) {
+      // file type has to be jpeg and file size has to be greater than 400 bytes (100 pixels)
+      const result = await uploadFile(file);
+      await unlinkFile(file.path);
+      try {
+        AWS.config.setPromisesDependency();
+        //AWS access details
+        AWS.config.update({
+          accessKeyId: process.env.AWS_ACCESS_KEY,
+          secretAccessKey: process.env.AWS_SECRET_KEY,
+          region: process.env.AWS_BUCKET_REGION,
+        });
 
-      const s3 = new AWS.S3();
-      const response = await s3
-        .listObjectsV2({
-          Bucket: process.env.AWS_BUCKET_NAME,
-        })
-        .promise();
-      // console.log(response);
-      // console.log("------------");
-      // console.log(response.Contents[0].Key);
-      // console.log("------------");
-      //input parameters
-      var params = {
-        Image: {
-          S3Object: {
-            Bucket: response.Name,
-            Name: response.Contents[0].Key,
+        const s3 = new AWS.S3();
+        const response = await s3
+          .listObjectsV2({
+            Bucket: process.env.AWS_BUCKET_NAME,
+          })
+          .promise();
+        // console.log(response);
+        // console.log("------------");
+        // console.log(response.Contents[0].Key);
+        // console.log("------------");
+        //input parameters
+        var params = {
+          Image: {
+            S3Object: {
+              Bucket: response.Name,
+              Name: response.Contents[0].Key,
+            },
           },
-        },
-      };
+        };
 
-      //Call AWS Rekognition Class
-      const rekognition = await new AWS.Rekognition();
+        //Call AWS Rekognition Class
+        const rekognition = await new AWS.Rekognition();
 
-      rekognition.recognizeCelebrities(params, function (err, data) {
-        if (err) console.log(err, err.stack);
-        // an error occurred
-        else {
-          // console.log("we got our celeb info back");
-          console.log(data);
-          res.send(data);
-        } // successful response
+        rekognition.recognizeCelebrities(params, function (err, data) {
+          if (err) console.log(err, err.stack);
+          // an error occurred
+          else {
+            // console.log("we got our celeb info back");
+            console.log(data);
+            res.json(data);
+          } // successful response
+        });
+      } catch (e) {
+        console.log("our error: " + e);
+      }
+    } else {
+      res.json({
+        status: 400,
+        error: "not an image/jpeg or file size is not larger than 100 pixels",
       });
-    } catch (e) {
-      console.log("our error: " + e);
     }
   } catch (e) {
     console.log(e);
@@ -162,7 +170,7 @@ app.get("/latestImage", async (req, res) => {
       else {
         // console.log("we got our celeb info back");
         console.log(data);
-        res.send(data);
+        res.json(data);
       } // successful response
     });
   } catch (e) {
